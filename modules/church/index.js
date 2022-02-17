7d626f3f0ea4df90faad79d677ecf2375c45fb03
andres1394s <andres1394s@gmail.com>
2022-02-17 16:53:17 -0500

    update

--- modules/church/index.js
+++ modules/church/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 const server = require('http').Server(app);
 server.listen(3000,'localhost', ()=> {
-    console.log('Server Started');
+    console.log('Server Started at http://localhost:3000');
 });
